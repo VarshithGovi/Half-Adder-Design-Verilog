e06d44bd28d0718df6e13518adabc0b1477cb1c0
Govindula Varshith <132640271+VarshithGovi@users.noreply.github.com>
2025-01-03 16:56:30 +0530

    Delete half_adder.v

--- half_adder.v
+++
@@ -1,9 +0,0 @@
-/* HA declaration
-saved in file half_adder.v*/
-
-module half_adder(a,b,sum, carry);
-    input a, b;
-    output sum, carry;
-    xor x1(sum, a, b);
-    and a1(carry, a, b);
-endmodule
